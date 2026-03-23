fce111bf3be3bfbb686bd99d483add3b56d268bc
janriva <janrigau2@gmail.com>
2026-03-23 10:14:21 +0100

    Update CellInPorusMedia3DSteppables.py

--- CellInPorusMedia3D/Simulation/CellInPorusMedia3DSteppables.py
+++ CellInPorusMedia3D/Simulation/CellInPorusMedia3DSteppables.py
@@ -8,7 +8,8 @@ class ConstraintInitializerSteppable(SteppableBasePy):
         SteppableBasePy.__init__(self,frequency)
 
     def start(self):
-        
+
+        #creating the plots
         self.plot_win = self.add_new_plot_window(title='Volume/Complexity',
                                                  x_axis_title='Volume',
                                                  y_axis_title='Complexity', x_scale_type='linear', y_scale_type='linear',
@@ -18,16 +19,18 @@ class ConstraintInitializerSteppable(SteppableBasePy):
         self.plot_win.save_plot_as_data("plots.txt",CSV_FORMAT)
         
         
-        self.flux = 1
-        
+        self.flux = 1 #1 for flux, 0 for not flux
+
+        #porus parameters
         wallWidth  =  2
         circleRadius= 20
         N_porus = 30
         dim = self.dim.x
+        #probability distribution parameters
         pics = 2
         sig = 50
         
-        #Posicions depenent de distribució de probabilitat
+        #creating the probability distribution (PD)
         dist_prob=np.zeros(dim)
         for pp in range(pics):
             # dist_prob += np.sinc(2*np.pi*(np.arange(0,dim)-pp*dim/pics))
@@ -37,15 +40,19 @@ class ConstraintInitializerSteppable(SteppableBasePy):
         dist_prob[:5] = 0
         dist_prob[-5:]=0
         dist_prob = dist_prob/dist_prob.sum()
+
+        #Selecting the porus positions based on PD
         pos_cent_x = np.random.choice(self.dim.x,size=N_porus, p = dist_prob,replace=False)
         pos_cent_y = np.random.choice(self.dim.y,size=N_porus, p = dist_prob,replace=False)
         pos_cent_z = np.random.choice(self.dim.z,size=N_porus, p = dist_prob,replace=False)
         
                
-        #Posicions aleatòries amb distribuicó uniforme
+        #If we want uniform distribution use next:
         # pos_cent_x = np.random.randint(0,self.dim.x, size = N_porus)
         # pos_cent_y = np.random.randint(0,self.dim.y, size = N_porus)
-        
+        # pos_cent_z = np.random.randint(0,self.dim.z, size = N_porus)
+
+        #Creating the porus media with one big wall cell
         newCell = self.new_cell(self.WALL)
         self.wall_points = []
         
@@ -53,6 +60,7 @@ class ConstraintInitializerSteppable(SteppableBasePy):
             dins = False
             if i==0 and j==0 and k==0:
                 continue
+            #cheking if the point it's inside a porus
             for nn in range(N_porus):
                 centx = int(pos_cent_x[nn])
                 centy = int(pos_cent_y[nn])
@@ -60,11 +68,12 @@ class ConstraintInitializerSteppable(SteppableBasePy):
                 if ((i-centx)**2 + (j-centy)**2+(k-centz)**2)<(circleRadius)**2:
                     dins = True
                     break
+            #if not inside create the wall and add the point to the list to keep track of it
             if not(dins):
                 self.cell_field[i,j,k] = newCell
                 self.wall_points.append([i,j,k,0])
                    
-        
+        #adding cells inside the centers of the porus
         for nn in range(N_porus):
             newCell2 = self.new_cell(self.CELL)
             centx = int(pos_cent_x[nn])
@@ -73,61 +82,36 @@ class ConstraintInitializerSteppable(SteppableBasePy):
             self.cell_field[centx-3:centx+3, centy-3:centy+3,centz-3:centz+3] = newCell2
                 
         
-        # for nn1 in range(N_porus):
-            # newCell2 = self.new_cell(self.CELL)
-            # # if nn1%2 ==0 :
-            # centx = int(pos_cent_x[nn1])
-            # centy = int(pos_cent_y[nn1])
-            # centz = int(pos_cent_z[nn1])
-            # self.cell_field[centx-3:centx+3, centy-3:centy+3,centz-3:centz+3] = newCell2
-                
-            # for dd in range(wallWidth):
-                # R = circleRadius+dd
-                # for ang in np.linspace(0,2*np.pi, 4*(int(R))):
-                    # for theta in np.linspace(0,np.pi, 4*(int(R))):
-                        
-                        # dins = False
-                        # px = int(centx+R* np.cos(ang)*np.sin(theta))
-                        # py = int(centy+R* np.sin(ang)*np.sin(theta))
-                        # pz = int(centz+R* np.cos(theta))
-
-                        # if (px+2<= self.dim.x-1 and px>=0) and (py+2<=self.dim.y-1 and py>=0) and (pz+2<=self.dim.z-1 and pz>=0):
-                            # for nn2 in range(N_porus):
-                                # if nn1!=nn2:
-                                    # if ((px-pos_cent_x[nn2])**2 + (py-pos_cent_y[nn2])**2+(pz-pos_cent_z[nn2])**2)<(circleRadius)**2:
-                                        # dins = True
-                                        # break
-                            # if not(dins):
-                                # self.cell_field[px:px+2,py:py+2,pz:pz+2] = newCell
-                                
-        
+                                     
+        #Creating initial nutrient field
         field = self.field.Nutr
-        field[:,:,:]=1 #aliment inicial
+        field[:,:,:]=1
            
-
+        #setting cells parameters
         for cell in self.cell_list:
 
             cell.targetVolume = 350
             cell.lambdaVolume = 2.0
         
     def step(self,mcs):
-        
+
         self.plot_win.erase_all_data()
         for cell in self.cell_list_by_type(self.CELL):
             if cell.volume < 10:
                 self.delete_cell(cell)
                 continue
-            #Wall degradetion    
+            #uncoment for Wall degradetion    
             # secretor = self.get_field_secretor("Degr")
             # secretor.secreteOutsideCellAtBoundary(cell, 0.7)            
-            
+
+            #saving complexity and volume parameters in the plot
             comp = cell.dict["complexity"]
             vol = cell.volume
             self.plot_win.add_data_point("VolComp", vol, comp)
             
             cell.lambdaVecY = 500
         
-        #Wall degradetion
+        #uncoment for Wall degradetion
         # field = self.field.Degr
         # nn = 0
         # for pp in self.wall_points:
@@ -145,14 +129,14 @@ class ConstraintInitializerSteppable(SteppableBasePy):
         # if mcs%10 == 0:
             # print(nn)
         
-        
+        # adding new nutrients (coment if not want it)
         field = self.field.Nutr
-        # field[:,:,:] = 1
-        field[0,:,:] = 1
+        # field[:,:,:] = 1 #constant nutrients
+        field[0,:,:] = 1 #adding nutrients coming for x = 0 plane
         
         
         
-        
+        #saving data as a txt in CSV format
         self.plot_win.save_plot_as_data("plots.txt",CSV_FORMAT)
         
 class GrowthSteppable(SteppableBasePy):
@@ -160,22 +144,23 @@ class GrowthSteppable(SteppableBasePy):
         SteppableBasePy.__init__(self, frequency)
         
     def start(self):
+        #creating the new information of cells
         for cell in self.cell_list_by_type(self.CELL):
             cell.dict["uptake"] = 0
             cell.dict["complexity"] = 0
 
     def step(self, mcs):          
-        
+        #make cells uptake
         secretor = self.get_field_secretor("Nutr")
         for cell in self.cell_list_by_type(self.CELL):
-            rat= 2-np.exp(-cell.dict["complexity"])
+            rat= 2-np.exp(-cell.dict["complexity"]) #more complex cells uptake more nutrients
             # arguments are: cell, max uptake, relative uptake
             # Um = secretor.uptakeInsideCellTotalCount(cell, 0.05*rat, 0.2)
             Um = secretor.uptakeOutsideCellAtBoundaryTotalCount(cell, 0.05*rat, 1)
             
             
             cell.dict["uptake"] += np.abs(Um.tot_amount)
-            
+            #make cells grow in volume or complexity when having enough nutrients saved
             if cell.dict["uptake"]>0.1:
                 gr_co = np.random.randint(0,2)
                 if gr_co == 0:                    
@@ -185,16 +170,6 @@ class GrowthSteppable(SteppableBasePy):
                     cell.dict["complexity"] += cell.dict["uptake"]
                     cell.dict["uptake"] = 0
             
-
-        # # alternatively if you want to make growth a function of chemical concentration uncomment lines below and comment lines above        
-
-        # field = self.field.CHEMICAL_FIELD_NAME
-        
-        # for cell in self.cell_list:
-            # concentrationAtCOM = field[int(cell.xCOM), int(cell.yCOM), int(cell.zCOM)]
-
-            # # you can use here any fcn of concentrationAtCOM
-            # cell.targetVolume += 0.01 * concentrationAtCOM       
 
         
 class MitosisSteppable(MitosisSteppableBase):
@@ -221,16 +196,8 @@ class MitosisSteppable(MitosisSteppableBase):
         self.parent_cell.targetVolume /= 2.0
 
         self.clone_parent_2_child()
-        
+        #dividing complexity as mitosis happen since organuls will split into parent and child
         self.parent_cell.dict["complexity"] /= 2.0
         self.child_cell.dict["complexity"] /= 2.0
-
-        # for more control of what gets copied from parent to child use cloneAttributes function
-        # self.clone_attributes(source_cell=self.parent_cell, target_cell=self.child_cell, no_clone_key_dict_list=[attrib1, attrib2]) 
-        
-        # if self.parent_cell.type==1:
-            # self.child_cell.type=1
-        # else:
-            # self.child_cell.type=1
 
         
